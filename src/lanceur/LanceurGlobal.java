8fac1f1d3e33baf805e55c3bb58a987628c9204a
Sageau <theo.deranchin@gmail.com>
2016-01-07 13:46:24 +0100

    Lanceur Global

---
+++ src/lanceur/LanceurGlobal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package lanceur;
+
+/**
+ * @author Théo
+ *
+ */
+public class LanceurGlobal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		LanceArene.main(args);
+		LanceIHM.main(args);
+	}
+
+}
